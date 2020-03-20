ada9c9b7968c676028c500320b2d5a9bdfa2ab5e
Sergey Skupow <sergskypow@gmail.com>
2020-03-20 19:58:59 +0200

    ui2

--- src/js/preloader.js
+++ src/js/preloader.js
@@ -6,18 +6,18 @@ menuBottom = document.querySelector('.menu'),
 section= document.getElementById('section-1'),
 panWrapper = document.querySelector('.pan-wrapper'),
 sectionBG = document.querySelector('.section-1-bg'),
+heroImg = document.querySelector('.hero-image'),
 balloon = document.querySelector('.parallax-el'),
 airLogo = document.querySelector('.air-logo');
 let tl = new gsap.timeline();
 document.addEventListener("DOMContentLoaded", ()=>{
   tl
-  .to(startImg,2,{scale:1.2,ease:Elastic.easeOut.config(1, 0.3),delay:0.5})
-  .to(plain,2.5,{y:"-100%", ease: Power4.easeOut,delay:0,stagger:0.1})
-  .from(bounce,2,{scale:1.2,x:0,opacity:0.8,ease: Power4.easeOut,delay:-2.7})
-  .from(menuTop,2,{y:"-100%",ease: Power4.easeOut,delay:-1.7})
-  //.from(menuBottom,2,{height:0,ease: Power4.easeOut,delay:-1.6})
-  .to(sectionBG,2.5,{scale:1.2,ease: Power4.easeOut,delay:-3.2})
-  .from(balloon,2.5,{y:800,ease: Power4.easeOut,delay:-3.2})
-  .from(airLogo,2.5,{y:1500,ease: Power4.easeOut,delay:-3.2})
+  .to(startImg,1,{scale:1.2,ease:Power4.easeOut})
+  .to(plain,1.5,{y:"-100%", ease: Power4.easeOut,delay:0,stagger:0.1})
+  .to(sectionBG,1,{'-webkit-filter':'blur('+(10)+'px)', 'filter':'blur('+(10)+'px)',ease: Power4.easeOut})
+  .to(heroImg,1,{scale:0.7,ease: Power4.easeOut,delay:-1})
+  //.from(menuTop,2,{y:"-100%",ease: Power4.easeOut,delay:-1.7})
+  .from(balloon,.5,{y:800,ease: Power4.easeOut,})
+  .from(airLogo,.5,{y:1000,ease: Power4.easeOut})
 })
 //Elastic.easeOut.config(1, 0.3)
