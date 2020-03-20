5ae8a6ca538cea75df4316d38afb77d430551ce9
Sergey Skupow <sergskypow@gmail.com>
2020-03-20 17:13:17 +0200

    -

--- src/js/animation.js
+++ src/js/animation.js
@@ -1,16 +1,19 @@
+const $ = document.querySelector.bind(document);
+const $on = document.addEventListener.bind(document);
 const bg = document.getElementById("image");
-bg.addEventListener("mousemove", event => mouseOver(event, bg,10));
-  bg.addEventListener("mouseleave", event => {
-    gsap.to(bg, 2, { x: 0, y: 0, ease: "elastic.out(1, 0.3)" });
-  });
-const balloonsArray = [...document.querySelectorAll(".balloon-type")];
+const balloonsArray = [...document.querySelectorAll(".balloon-hover")];
+const blocks = [...document.querySelectorAll(".img-block")];
+
+if(window.innerWidth>1440){
 balloonsArray.forEach(item => {
   item.addEventListener("mousemove", event => mouseOver(event, item));
   item.addEventListener("mouseleave", event => {
     gsap.to(item, 2, { x: 0, y: 0, ease: "elastic.out(1, 0.3)" });
+    gsap.to(item.querySelector('img'), 2, { x: 0, y: 0,scale:1, ease: "elastic.out(1, 0.3)" });
   });
 });
-function mouseOver(event, item,value=2) {
+}
+function mouseOver(event, item, value = 2) {
   let rect = item.getBoundingClientRect();
   let pX = (event.clientX - rect.left).toFixed();
   let pY = (event.clientY - rect.top).toFixed();
@@ -22,22 +25,93 @@ function mouseOver(event, item,value=2) {
     y: (pY - cY) / value,
     ease: "power3.out"
   });
+  gsap.to(item.querySelector('img'), 1, {
+    x: (pX - cX) / value/2,
+    y: (pY - cY) / value/2,
+    scale:1.2,
+    ease: "power3.out"
+  });
 }
 
 ////////skew animation///////////
 const img = document.querySelector(".img-container"),
   text = document.querySelector(".inner-right-wrapper"),
-  section = document.querySelectorAll('.section-bg');
+  section = document.querySelectorAll(".section-bg-skew");
 
 let currentPixel = window.pageYOffset;
 
-const looper = function() {
-  let newPixel = window.pageYOffset;
-  let diff = newPixel - currentPixel;
-  let speed = diff * 0.05;
-  gsap.to(section, 1, { skewY: speed ,ease: "elastic.out(1, 0.3)"});
-  //gsap.to(text, 1, { skewY: speed });
-  requestAnimationFrame(looper);
-  currentPixel = newPixel;
-};
-looper();
+// const looper = function() {
+//   let newPixel = window.pageYOffset;
+//   let diff = newPixel - currentPixel;
+//   let speed = diff * 0.05;
+//   gsap.to(section, 1, { skewY: speed, ease: "elastic.out(1, 0.3)" });
+//   //gsap.to(text, 1, { skewY: speed });
+//   requestAnimationFrame(looper);
+//   currentPixel = newPixel;
+// };
+// looper();
+
+////////////////////////Cursor////////////////////////////////////////
+let cursor = $(".cursor");
+let posX = 0,
+  posY = 0,
+  mouseX = 0,
+  mouseY = 0;
+
+gsap.to({}, 0.016, {
+  repeat: -1,
+  onRepeat: function() {
+    posX += (mouseX - posX) / 9;
+    posY += (mouseY - posY) / 9;
+
+    // gsap.set(follower, {
+    //   css: {
+    //     left: posX - 20,
+    //     top: posY - 20
+    //   }
+    // });
+
+    gsap.to(cursor, {
+      css: {
+        left: mouseX-cursor.offsetWidth/2,
+        top: mouseY-cursor.offsetHeight/2
+      },
+      ease: Power4.easeOut
+    });
+  }
+});
+
+document.addEventListener("mousemove", function(e) {
+  mouseX = e.pageX;
+  mouseY = e.pageY;
+});
+const balloonsURIS = [...document.querySelectorAll(".cursor-change")];
+if(window.innerWidth>1440){
+  balloonsURIS.forEach(item => {
+    item.addEventListener("mouseenter", function() {
+    cursor.classList.add("active");
+    //follower.classList.add("active");
+   
+    item.addEventListener("mouseleave", function() {
+    cursor.classList.remove("active");
+    //follower.classList.remove("active");
+    });
+  });
+});
+}
+
+////////////////////////Cursor////////////////////////////////////////
+
+///////////////////////Menu/////////////////////////////////////////
+// let prevScrollPos = window.pageYOffset;
+// window.onscroll = ()=>{
+//   let currentScrollPos = window.pageYOffset;
+//   if(prevScrollPos>currentScrollPos){
+//     document.querySelector('.menu').style.top = "0";
+//   }
+//   else{
+//     document.querySelector('.menu').style.top = "-100px";
+//   }
+//   prevScrollPos = currentScrollPos;
+// }
+///////////////////////Menu/////////////////////////////////////////
--- src/js/preloader.js
+++ src/js/preloader.js
@@ -1,12 +1,23 @@
-const plain = document.getElementById('plain'),
-plain2 = document.getElementById('plain-2'),
+const plain = [...document.querySelectorAll(".plain")],
 bounce = document.getElementById('bounce'),
-startImg = document.getElementById('start-img');
+startImg = document.getElementById('start-img'),
+menuTop = document.querySelector('.interface'),
+menuBottom = document.querySelector('.menu'),
+section= document.getElementById('section-1'),
+panWrapper = document.querySelector('.pan-wrapper'),
+sectionBG = document.querySelector('.section-1-bg'),
+balloon = document.querySelector('.parallax-el'),
+airLogo = document.querySelector('.air-logo');
 let tl = new gsap.timeline();
 document.addEventListener("DOMContentLoaded", ()=>{
   tl
-  .from(startImg,2,{scale:0.7,ease:Power4.easeOut,delay:0.5})
-  .to([plain,plain2],2,{y:"-100%", ease: Power4.easeOut,delay:0,stagger:0.08})
-  .from(bounce,1.5,{scale:1.2,x:0,opacity:0.8,ease: Power4.easeOut,delay:-2})
+  .to(startImg,2,{scale:1.2,ease:Elastic.easeOut.config(1, 0.3),delay:0.5})
+  .to(plain,2.5,{y:"-100%", ease: Power4.easeOut,delay:0,stagger:0.1})
+  .from(bounce,2,{scale:1.2,x:0,opacity:0.8,ease: Power4.easeOut,delay:-2.7})
+  .from(menuTop,2,{y:"-100%",ease: Power4.easeOut,delay:-1.7})
+  //.from(menuBottom,2,{height:0,ease: Power4.easeOut,delay:-1.6})
+  .to(sectionBG,2.5,{scale:1.2,ease: Power4.easeOut,delay:-3.2})
+  .from(balloon,2.5,{y:800,ease: Power4.easeOut,delay:-3.2})
+  .from(airLogo,2.5,{y:1500,ease: Power4.easeOut,delay:-3.2})
 })
 //Elastic.easeOut.config(1, 0.3)
